34f832b641277f03e49f129bd29c76b6a4329a6a
Márton Lipécz <mlipecz@gmail.com>
2018-08-28 03:22:52 +0200

    hello2

--- src/main/java/com/codecool/shitshop/orderservice/controller/HelloWorld.java
+++ src/main/java/com/codecool/shitshop/orderservice/controller/HelloWorld.java
@@ -6,16 +6,11 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.*;
 
 @Controller
-@SpringBootApplication
 public class HelloWorld {
 
     @RequestMapping("/")
     @ResponseBody
     String home() {
         return "Hello World!";
-    }
-
-    public static void main(String[] args) {
-        SpringApplication.run(HelloWorld.class, args);
     }
 }
